a3fa7f0e2670f2bea9706bcc230a910afe8f7d85
Taichi I <fng.trf.kk1020@gmail.com>
2021-03-09 00:19:28 -0800

    add render function

--- currentWeather.js
+++ currentWeather.js
@@ -4,16 +4,21 @@ let city = 'Vancouver';
 let timer = null;
 let notFound = false;
 let added = false;
+// const refreshInterval = 3000;
+const refreshInterval = 120000;
+let dayCounter = 1;
+
 const APIkey = 'aae34dd3fa439d5cb44c308cae049f36';
 const baseUrl = 'https://api.openweathermap.org/data/2.5/';
+
 const searchBox = document.getElementById('searchbox');
-const refreshInterval = 3000;
-// const refreshInterval = 120000;
+const place = document.getElementById('place');
+const weatherContainer = document.getElementById('weather-container');
 
 const startFetch = async () => {
   notFound = false;
   await fetchAPI(city);
-  if(!notFound) {
+  if (!notFound) {
     timer = setInterval(() => {
       fetchAPI(city);
     }, refreshInterval);    
@@ -29,27 +34,60 @@ const searchFunc = (e) => {
 };
 
 const renderData = (data) => {
-  // const name = data.city.name;
-  // const country = data.city.country;
+  place.innerHTML = '';
+  weatherContainer.innerHTML = '';
 
-  // data.list.forEach((forecast, index) => {
-  //   const weather = forecast.weather[0].main;
-  //   const description = forecast.weather[0].description;
-  //   const feelLike = Math.round(forecast.main.feels_like);
-  //   const temp = Math.round(forecast.main.temp);
-  //   const tempMax = Math.round(forecast.main.temp_max);
-  //   const tempMin = Math.round(forecast.main.temp_min);
-  //   const humidity = forecast.main.humidity;  
-  // });
+  const name = data.city.name;
+  const country = data.city.country;
+  document.getElementById('place').innerHTML = `${name}, ${country}`;
 
-  // const currentWeather = `
-  //   <div>
-  //     <p>${weather}</p>
-  //     <p>${currentTemp}</p>
-  //   </div>
-  // `;
-  // document.getElementById('place').innerHTML = `${name}, ${country}`;
-  // document.getElementById('current-weather').innerHTML = currentWeather;
+  data.list.forEach((forecast, index) => {
+    const dateTime = new Date(forecast.dt * 1000);
+    const month = dateTime.getMonth() + 1;
+    const date = dateTime.getDate();
+    const hours = dateTime.getHours();
+    const min = String(dateTime.getMinutes()).padStart(2, '0');
+    const weather = forecast.weather[0].main;
+    const description = forecast.weather[0].description;
+    const feelLike = Math.round(forecast.main.feels_like);
+    const temp = Math.round(forecast.main.temp);
+    const tempMax = Math.round(forecast.main.temp_max);
+    const tempMin = Math.round(forecast.main.temp_min);
+    const humidity = forecast.main.humidity;  
+    const icon = `images/${forecast.weather[0].icon}.png`;
+
+    if(index % 8 === 0) { // Now or after 24x hours
+      const oneDayWrapper = `
+        <div class="oneday-wrapper-${dayCounter}" id="oneday-wrapper">
+          <div class="current-weather">
+            <img src="icon"></img>
+            <div>
+              <div>${weather}<span>${description}</span></div>
+              <div class="current-weather__temp">
+                ${temp}
+                <div>
+                  <div>H:${tempMax} L:${tempMin} Feel like: ${feelLike}</div>
+                  <div>Humidity: ${humidity}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="forecast-list-${dayCounter}"></div>
+        </div>
+      `;
+      document.getElementById('weather-container').insertAdjacentHTML('beforeend', oneDayWrapper);
+    } else {
+      const onedayForecast = `
+        <div class="forecast-hour">
+          <div>${hours}</div>
+          <img src="${icon}"></img>
+          <div>${temp}</div>
+        </div>
+      `;
+      document.getElementById(`forecast-list-${dayCounter}`).insertAdjacentHTML('beforeend', onedayForecast);
+      index % 8 === 7 && dayCounter++;
+    }
+  });
 };
 
 const fetchAPI = async (cityName) => {
